f305e895409491d5362410c9a74fe1afc5a36dd2
Eduardo Borsa <borsa.dado@gmail.com>
2020-08-15 19:14:20 -0300

    Initial Commit
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

---
+++ lib/pathfinder.ex
@@ -0,0 +1,18 @@
+defmodule Pathfinder do
+  @moduledoc """
+  Documentation for `Pathfinder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pathfinder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
